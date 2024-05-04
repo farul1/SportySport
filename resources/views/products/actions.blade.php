0dc625d06364c044a585e6a55dcd7f38f6f00673
farulSP <72846589+farul1@users.noreply.github.com>
2024-05-04 10:44:31 +0700

    Update actions.blade.php

--- resources/views/products/actions.blade.php
+++ resources/views/products/actions.blade.php
@@ -1,9 +1,9 @@
 <div class="btn-group" role="group" aria-label="Basic example">
-    <a href="{{ route('products.show', $product->id) }}" class="btn btn-info" title="Lihat"><i class="bi bi-eye"></i> Lihat</a>
-    <a href="{{ route('products.edit', $product->id) }}" class="btn btn-warning" title="Edit"><i class="bi bi-pencil"></i> Edit</a>
-    <form action="{{ route('products.destroy', $product->id) }}" method="POST" style="display: inline;">
+    <a href="{{ route('products.show', $product->id) }}" class="btn3" title="Lihat"><i class="bi bi-eye">show</i></a>
+    <a href="{{ route('products.edit', $product->id) }}" class="btn3" title="Edit"><i class="bi bi-pencil">edit</i></a>
+    <form action="{{ route('products.destroy', $product->id) }}" method="POST">
         @csrf
         @method('DELETE')
-        <button type="submit" class="btn btn-danger" onclick="return confirm('Anda yakin ingin menghapus produk ini?')" title="Hapus"><i class="bi bi-trash"></i> Hapus</button>
+        <button type="submit" class="btn3" onclick="return confirm('Anda yakin ingin menghapus produk ini?')" title="Hapus"><i class="bi bi-trash"></i>delete</button>
     </form>
 </div>
